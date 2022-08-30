75fe4ed3da3f99a3fd90d086da4095101beb9d76
DmitryFedoroff <fedoroffx@gmail.com>
2022-08-30 23:32:22 +0700

    feat: Print contents of array

--- test-july-2022/Program.cs
+++ test-july-2022/Program.cs
@@ -1,1 +1,10 @@
-﻿string[] array = { "hello", "2", "world", ":-)" };
+﻿void Print(string[] array)
+{
+    foreach (string element in array)
+    {
+        Console.Write(element + " ");
+    }
+    Console.WriteLine();
+}
+
+string[] array = { "hello", "2", "world", ":-)" };
