fe999b32e7e2f8b40155ec772795fe469f6ee88d
DmitryFedoroff <fedoroffx@gmail.com>
2022-09-02 14:54:43 +0700

    feat: Read characters from input stream and split input string based on delimiters

--- test-july-2022-advanced/Program.cs
+++ test-july-2022-advanced/Program.cs
@@ -1,2 +1,21 @@
-﻿string[] array = ReadInputStream("Введите элементы массива: ");
+﻿string[] ReadInputStream(string msg)
+{
+ReInput:
+    Console.Write(msg);
+    string? str = Console.ReadLine();
+    if (str != String.Empty && str != " ")
+    {
+        char[] splitChars = { ' ', ',' };
+        string[] inputArray = str.Split(splitChars, StringSplitOptions.RemoveEmptyEntries);
+        return inputArray;
+    }
+    else
+    {
+        Console.WriteLine("Вы ничего не ввели!");
+        Console.WriteLine();
+        goto ReInput;
+    }
+}
+
+string[] array = ReadInputStream("Введите элементы массива: ");
 int charLim = 3;
