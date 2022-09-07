82c3d8ad3fd58506cba5dd9c9a00c8866ea40343
DmitryFedoroff <fedoroffx@gmail.com>
2022-09-07 10:41:12 +0700

    feat: Assign random integer for each element of matrix

--- sum-of-matrix-main-diagonal/Program.cs
+++ sum-of-matrix-main-diagonal/Program.cs
@@ -11,6 +11,18 @@
     Console.WriteLine();
 }
 
+void Fill(int[,] matr)
+{
+    Random rand = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rand.Next(0, 10);
+        }
+    }
+}
+
 Console.Write("Enter number of matrix rows: ");
 int n = int.Parse(Console.ReadLine());
 Console.Write("Enter number of matrix columns: ");
