85eced7f269fb4d1e7120250ed861a587c919add
DmitryFedoroff <fedoroffx@gmail.com>
2022-09-08 15:25:59 +0700

    feat: Sort elements of array in wave form

--- sort-array-in-wave-form/Program.cs
+++ sort-array-in-wave-form/Program.cs
@@ -20,6 +20,23 @@ void Swap(int[] arr, int a, int b)
     arr[b] = temp;
 }
 
+int[] SortWave(int[] arr)
+{
+    int len = arr.Length;
+    for (int i = 0; i < len; i+= 2)
+    {
+        if (i > 0 && arr[i - 1] > arr[i])
+        {
+            Swap(arr, i - 1, i);
+        }
+        if (i < len - 1 && arr[i] < arr[i + 1])
+        {
+            Swap(arr, i, i + 1);
+        }
+    }
+    return arr;
+}
+
 int[] array = new int[10];
 Fill(array);
 Console.Write("Unsorted array: ");
