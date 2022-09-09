bbc31590f5d645958d7635c96ab28459826df930
DmitryFedoroff <fedoroffx@gmail.com>
2022-09-09 11:19:18 +0700

    feat: Multiply two matrices

--- multiply-two-matrices/Program.cs
+++ multiply-two-matrices/Program.cs
@@ -22,6 +22,29 @@ void Fill(int[,] matrx)
     }
 }
 
+int [,] Multiply(int[,] matrixA, int[,] matrixB)
+{
+    int rowA = matrixA.GetLength(0);
+    int columnA = matrixA.GetLength(1);
+    int rowB = matrixB.GetLength(0);
+    int columnB = matrixB.GetLength(1);
+    int temp = 0;
+    int[,] matrixC = new int[rowA, columnB];
+    for (int i = 0; i < rowA; i++)
+    {
+        for (int j = 0; j < columnB; j++)
+        {
+            temp = 0;
+            for (int k = 0; k < columnA; k++)
+            {
+                temp += matrixA[i, k] * matrixB[k, j];
+            }
+            matrixC[i, j] = temp;
+        }
+    }
+    return matrixC;
+}
+
 Console.Write("Enter number of matrix rows: ");
 int n = int.Parse(Console.ReadLine());
 Console.Write("Enter number of matrix columns: ");
