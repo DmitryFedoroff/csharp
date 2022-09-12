9dfa08dc03c5ca0519deb86e4b7acee18be1d673
DmitryFedoroff <fedoroffx@gmail.com>
2022-09-12 20:35:24 +0700

    feat: Convert decimal number into binary number

--- convert-decimal-to-binary/Program.cs
+++ convert-decimal-to-binary/Program.cs
@@ -10,6 +10,18 @@
     return count;
 }
 
+int[] ConvertToBinary(int[] arr, int num)
+{
+    int i = 0;
+    while (num != 0)
+    {
+        arr[i] = num % 2;
+        num = num / 2;
+        i++;
+    }
+    return arr;
+}
+
 Console.Write("Enter decimal number to convert: ");
 int num = int.Parse(Console.ReadLine());
 int[] array = new int[FindArrSize(num)];
