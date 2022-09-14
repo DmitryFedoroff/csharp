8f5aae027331a196c4c1d5538168f1528612a1c4
DmitryFedoroff <fedoroffx@gmail.com>
2022-09-14 10:28:58 +0700

    feat: Assign random integer for each element of two-dimensional array

--- replace-even-odd-positioned-elements-2d-array/Program.cs
+++ replace-even-odd-positioned-elements-2d-array/Program.cs
@@ -11,6 +11,18 @@
     Console.WriteLine();
 }
 
+void Fill(int[,] arr)
+{
+    Random rand = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rand.Next(0, 10);
+        }
+    }
+}
+
 Console.Write("Enter number of array rows: ");
 int n = int.Parse(Console.ReadLine());
 Console.Write("Enter number of array columns: ");
