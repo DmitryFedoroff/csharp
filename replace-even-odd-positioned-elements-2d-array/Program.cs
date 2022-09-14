642c4d099e92384b44985646d67d83cfb53f9216
DmitryFedoroff <fedoroffx@gmail.com>
2022-09-14 10:12:20 +0700

    dc: Create two-dimensional array of user-defined size

---
+++ replace-even-odd-positioned-elements-2d-array/Program.cs
@@ -0,0 +1,6 @@
+﻿﻿Console.Write("Enter number of array rows: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Enter number of array columns: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine();
+int[,] array = new int[n, m];
