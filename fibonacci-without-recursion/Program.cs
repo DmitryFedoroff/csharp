f5fecf096423f1003561471d91a40bcd7688d498
DmitryFedoroff <fedoroffx@gmail.com>
2022-09-15 19:40:13 +0700

    feat: Print contents of array

--- fibonacci-without-recursion/Program.cs
+++ fibonacci-without-recursion/Program.cs
@@ -1,3 +1,12 @@
-﻿Console.Write("Enter length of Fibonacci series: ");
+﻿﻿void Print(int[] fiboNums)
+{
+    foreach (int number in fiboNums)
+    {
+        Console.Write(number + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Enter length of Fibonacci series: ");
 int numsLimit = int.Parse(Console.ReadLine());
 int[] array = new int[numsLimit];
