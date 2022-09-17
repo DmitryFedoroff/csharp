fb6ccac0e82e79b87d7231a000d50c86b6b07a16
DmitryFedoroff <fedoroffx@gmail.com>
2022-09-17 20:10:01 +0700

    dc: Create two-dimensional array of user-defined size

---
+++ remove-row-column-intersects-smallest-element-2d-array-v2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Enter number of array rows: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Enter number of array columns: ");
+int m = int.Parse(Console.ReadLine());
+int[,] array = new int[n, m];
+Console.WriteLine();
