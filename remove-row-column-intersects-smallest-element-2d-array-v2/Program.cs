f31f0cf37366ac548e453210672fb9728f094f92
DmitryFedoroff <fedoroffx@gmail.com>
2022-09-17 20:12:57 +0700

    feat: Print contents of two-dimensional array

--- remove-row-column-intersects-smallest-element-2d-array-v2/Program.cs
+++ remove-row-column-intersects-smallest-element-2d-array-v2/Program.cs
@@ -1,4 +1,17 @@
-﻿Console.Write("Enter number of array rows: ");
+﻿void Print(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Enter number of array rows: ");
 int n = int.Parse(Console.ReadLine());
 Console.Write("Enter number of array columns: ");
 int m = int.Parse(Console.ReadLine());
