b843f84971f81bbb64c8cc4db8e102c619be6240
DmitryFedoroff <fedoroffx@gmail.com>
2022-09-17 20:21:09 +0700

    feat: Assign random integer for each element of two-dimensional array

--- remove-row-column-intersects-smallest-element-2d-array-v2/Program.cs
+++ remove-row-column-intersects-smallest-element-2d-array-v2/Program.cs
@@ -11,6 +11,20 @@
     Console.WriteLine();
 }
 
+void Fill(int[,] arr)
+{
+    int rowLen = arr.GetLength(0);
+    int colLen = arr.GetLength(1);
+    Random rand = new Random();
+    for (int i = 0; i < rowLen; i++)
+    {
+        for (int j = 0; j < colLen; j++)
+        {
+           arr[i, j] = rand.Next(0, 10);
+        }
+    }
+}
+
 Console.Write("Enter number of array rows: ");
 int n = int.Parse(Console.ReadLine());
 Console.Write("Enter number of array columns: ");
