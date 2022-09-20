419903b595df345078f96c4a71181e08fd143b07
DmitryFedoroff <fedoroffx@gmail.com>
2022-09-20 22:59:24 +0700

    feat: Check if lines are parallel

--- draw-intersection-between-two-lines-in-console/Program.cs
+++ draw-intersection-between-two-lines-in-console/Program.cs
@@ -1,4 +1,13 @@
-﻿Console.WriteLine("Enter data to find intersection point of two lines.");
+﻿void CheckParallel(int k1, int k2)
+{
+    if (k1 == k2)
+    {
+        Console.WriteLine("The lines are parallel. There is no point\u001B[32m ⦿ \u001b[0m of intersection."); // ANSI .NET color green \u001B[32m       
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Enter data to find intersection point of two lines.");
 Console.WriteLine("For example, b1 = 2, k1 = 5, b2 = 4, k2 = 9");
 Console.Write("Enter b1: ");
 int b1 = int.Parse(Console.ReadLine());
