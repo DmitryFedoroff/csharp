5313c4cc7a80edde78f7ef854395b6548ee07e30
DmitryFedoroff <fedoroffx@gmail.com>
2022-09-26 22:12:46 +0700

    feat: Find total number of unique sums of pairs possible from elements of array

--- count-distinct-pair-sum/Program.cs
+++ count-distinct-pair-sum/Program.cs
@@ -17,6 +17,20 @@ void Fill(int[] arr)
     }
 }
 
+void FindUniqSums(int []arr)
+{
+    HashSet<int> s = new HashSet<int>();
+    int len = arr.Length;
+    for (int i = 0; i < len - 1; i++)
+    {
+        for (int j = i + 1; j < len; j++)
+        {
+            s.Add(arr[i] + arr[j]);
+        }
+    }
+    Console.WriteLine(s.Count);
+}
+
 int[] array = new int[4];
 Fill(array);
 Console.Write("Original array: ");
